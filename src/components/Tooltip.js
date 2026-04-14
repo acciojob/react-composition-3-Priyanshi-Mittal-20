df625a4e334dd9cf184f66ab019eabafabf449ad
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-04-14 18:23:02 +0530

    react-4

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -5,11 +5,13 @@ import { useState } from "react";
 const Tooltip=({fun1,fun2,fun3,fun4,text,children})=>{
   return (
     <div>
-      <h2 className="tooltip" onMouseOver={fun1} onMouseLeave={fun3}><span>Hover over me</span></h2>
-      {text && <div className="tooltiptext">{text}</div>}
-      <br/><hr/>
-      <p className="tooltip" onMouseOver={fun2} onMouseLeave={fun4}><span>Hover over me to see another</span>tooltip</p>
-      {children && <div className="tooltiptext">{children}</div>}
+      <h2 className="tooltip" onMouseEnter={fun1} onMouseLeave={fun3}>
+        Hover over me
+        {text && <div className="tooltiptext">{text}</div>}</h2>
+      <hr/><br/>
+      <p className="tooltip" onMouseEnter={fun2} onMouseLeave={fun4}>Hover over me to see another tooltip
+        {children && <div className="tooltiptext">{children}</div>}
+      </p>
       <hr/><br/>
     </div>
   )
