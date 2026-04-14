e5a5684eb032faad3fc6cfc527a806aea3835dc5
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-04-14 18:47:07 +0530

    react-7

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -9,10 +9,10 @@ const Tooltip=({fun1,fun2,fun3,fun4,text,children})=>{
     <div>
       <h2 className="tooltip" onMouseOver={()=>{fun1();setShow1(true)}} onMouseLeave={()=>{fun3();setShow1(false)}}>
         Hover over me
-        {show1 && <div className="tooltiptext">{text}</div>}</h2>
+        {show1 && <div className="tooltiptext">This is a tooltip</div>}</h2>
       <hr/><br/>
       <p className="tooltip" onMouseOver={()=>{fun2();setShow2(true)}} onMouseLeave={()=>{fun4();setShow2(false)}}>Hover over me to see another tooltip
-        {show2 && <div className="tooltiptext">{children}</div>}
+        {show2 && <div className="tooltiptext">This is another tooltip</div>}
       </p>
       <hr/><br/>
     </div>
