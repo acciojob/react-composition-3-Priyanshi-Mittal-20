8efd3ed7732e803e22483f9948e4c4022a42a42e
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-04-23 11:30:48 +0530

    react-11

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -4,29 +4,15 @@ import Tooltip from "./Tooltip";
 
 
 const Parent=()=>{
-  const [text,setText]=useState("")
-  const [children,setChildren]=useState("")
-  function handleTooltip1()
-  {
-    setText("This is a tooltip")
-  }
-  function handleTooltip2()
-  {
-    setChildren("This is another tooltip")
-  }
-  function handleTooltip1_1()
-  {
-    setText("")
-  }
-  function handleTooltip2_1()
-  {
-    setChildren("")
-  }
-  return(
-    <div>
-      <Tooltip fun1={handleTooltip1} fun2={handleTooltip2} fun3={handleTooltip1_1} fun4={handleTooltip2_1} text={text} children={children}/>
-    </div>
-  )
+  <>
+  <Tooltip text="This is a tooltip">
+  <h2>Hover over me</h2>
+</Tooltip>
+
+<Tooltip text="This is another tooltip">
+  <p>Hover over me to see another tooltip</p>
+</Tooltip>
+</>
 }
 
 
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,23 +2,21 @@ import React from "react";
 import { useState } from "react";
 
 
-const Tooltip=({fun1,fun2,fun3,fun4,text,children})=>{
-  const [show1,setShow1]=useState(false)
-  const [show2,setShow2]=useState(false)
+const Tooltip = ({ text, children }) => {
+  const [show, setShow] = useState(false);
+
   return (
-    <div>
-      <h2 className="tooltip" onMouseEnter={() => setShow1(true)} onMouseLeave={() => setShow1(false)}>
-          Hover over me
-        {show1 && <div className="tooltiptext">This is a tooltip</div>}</h2>
-      <hr/><br/>
-      <p className="tooltip" onMouseEnter={() => setShow2(true)} onMouseLeave={() => setShow2(false)}>
-          Hover over me to see another tooltip
-        {show2 && <div className="tooltiptext">This is another tooltip</div>}
-      </p>
-      <hr/><br/>
+    <div
+      className="tooltip"
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+    >
+      {children}
+      {show && <div className="tooltiptext">{text}</div>}
     </div>
-  )
-}
+  );
+};
+
 
 
 
